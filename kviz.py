a5c95fe88b377a6e80a8320d973c8d7ad0c544c3
Urban Merhar <merhar.urban@gmail.com>
2019-07-31 15:01:45 +0200

    Izmislil sem si nekaj vprasanj za kviz.

---
+++ kviz.py
@@ -0,0 +1,14 @@
+#Vprašanja bodo našteta v obliki v obliki: 'vprašanje':[možnosti]
+#Kaj je '___'?
+vprasanja = {
+    'Ama':['Japonska nabiralka biserov', 'Ena izmed Bondovih deklet', 'Otok v Pacifiku', 'Tibetansko govedo'],
+    'paradižnik':['Sadje', 'Zelenjava', 'Meso', 'Goba'],
+    'Mars':['Planet in rimski bog vojne', 'Samo planet', 'Planet in grški bog vojne', 'Planet in etruščanski bog'],
+    'Jamb':['Vrsta ritma in igra', 'Vrsta ritma', 'Igra', 'Igra in pripovedno delo'],
+    'State of Washington':['Zvezna država v ZDA', 'Glavno mesto ZDA', 'Nekdanji predsednik ZDA', 'Samostojna država'],
+    'Jak':['Tibetansko govedo', 'Vrsta bizona', 'Afriška volovska antilopa', 'Azijska divja koza'],
+    'Fobos':['Marsov naravni satelit', 'Jupitrov naravni satelit', 'Venerin naravni satelit', 'Zemljin naravni satelit' ],
+    'Cerera':['Rimska boginja ljubezni in pritlikavi planet', 'Samo rimska boginja ljubezni', 'Egipčanska boginja neba', 'Grška boginja zdravilstva'],
+    'bil vzdevek nemškega generalfeldmaršala Erwina Rommla':['Puščavska lisica', 'Afriški lev', 'Puščavski tiger', 'Nemški panter'],
+    'Tar':['Azijska divja koza', 'Tibetansko govedo', 'Afriška volovska antilopa', 'Pritlikav indonezijski konj']
+}
