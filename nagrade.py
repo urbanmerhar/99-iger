2ff991040a0aed9b6cd745df203c6073b2f2f864
Urban Merhar <merhar.urban@gmail.com>
2019-08-03 16:34:13 +0200

    Ustvaril seznam šal in zaniivosti, za različne nagrade.

--- nagrade.py
+++ nagrade.py
@@ -1,11 +1,18 @@
 #Nagrade za zmage pri igrah.
 import random
 
-seznam_nagrad = [
+seznam_sal = [
     'Kaj dobiš, če daš dva polža skupaj? Celža.',
-    'Fobos je Marsov naravni satelit, ki ima nestabilno orbito in se zato na približno vsakih 100 let Marsu približa za 9m.'
+    'ŠALA'
     
+]
+
+seznam_zanimivosti = [
+    'Fobos je Marsov naravni satelit, ki ima nestabilno orbito in se zato na približno vsakih 100 let Marsu približa za 9m.'
+
 ]
 
 def vrni_nagrado(seznam):
     return random.choice(seznam)
+
+
