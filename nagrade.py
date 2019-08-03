ef354ccec12f0eeafb0dcd5c9b25882cb6f7191f
Urban Merhar <merhar.urban@gmail.com>
2019-08-03 10:25:31 +0200

    Datoteka z nagradami in funkcija za naključno izbiro nagrade.

--- nagrade.py
+++ nagrade.py
@@ -1,6 +1,10 @@
 #Nagrade za zmage pri igrah.
+import random
 
 seznam_nagrad = [
-    'Kaj dobiš, če daš dva polža skupaj? Celža.',
+    'Kaj dobiš, če daš dva polža skupaj? Celža.'
     
 ]
+
+def vrni_nagrado(seznam):
+    return random.choice(seznam)
