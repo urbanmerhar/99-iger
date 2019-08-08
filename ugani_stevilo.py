6e1dbd3e7b9758fedb60938e04b8580f8f9594b0
Urban Merhar <merhar.urban@gmail.com>
2019-08-08 11:03:55 +0200

    Posodobitev modela igre.

--- ugani_stevilo.py
+++ ugani_stevilo.py
@@ -2,17 +2,16 @@ import random
 
 class Ugani_stevilo:
     def __init__(self):
-        self.iskano_stevilo = None
         self.trenutno_stevilo = None
         self.kolikokrat_smo_ze_ugibali = 0
         a = random.randint(-10000, 10000)
         b = random.randint(-10000, 10000)
-        if a < b:
-            self.spodnja_meja = a
-            self.zgornja_meja = b
-        elif a >= b:
-            self.spodnja_meja = b
-            self.zgornja_meja = a
+        c = random.randint(-10000, 10000)
+        self.spodnja_meja = min(a, b, c)
+        self.zgornja_meja = max(a, b, c)
+        self.iskano_stevilo = a + b + c - self.spodnja_meja - self.zgornja_meja
+        #Pogoj še čekiraj kako deluje.
+        self.pogoj_za_nagrado = (self.spodnja_meja + self.zgornja_meja) // 4
         
         
 
