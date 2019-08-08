3048e9aaf7ec625c4fcd9ca18fd4d8a98a6f38fd
Urban Merhar <merhar.urban@gmail.com>
2019-08-08 11:36:13 +0200

    Spremenil dve vprašanji.

--- nakljucni_kviz.py
+++ nakljucni_kviz.py
@@ -6,14 +6,14 @@ import copy
 #Kaj je '___'?
 vprasanja_za_kviz = {
     'Ama':['Japonska nabiralka biserov', 'Ena izmed Bondovih deklet', 'Otok v Pacifiku', 'Tibetansko govedo'],
-    'paradižnik':['Sadje', 'Zelenjava', 'Meso', 'Goba'],
+    'je dosegel Vasco de Gama':['Obplu Afriko in odkril pot do Indije', 'Obplu svet', 'Pristal v Južni Ameriki', 'Odkril Azore'],
     'Mars':['Planet in rimski bog vojne', 'Samo planet', 'Planet in grški bog vojne', 'Planet in etruščanski bog'],
     'Jamb':['Vrsta ritma in igra', 'Vrsta ritma', 'Igra', 'Igra in pripovedno delo'],
     'State of Washington':['Zvezna država v ZDA', 'Glavno mesto ZDA', 'Nekdanji predsednik ZDA', 'Samostojna država'],
     'Jak':['Tibetansko govedo', 'Vrsta bizona', 'Afriška volovska antilopa', 'Azijska divja koza'],
     'Fobos':['Marsov naravni satelit', 'Jupitrov naravni satelit', 'Venerin naravni satelit', 'Zemljin naravni satelit' ],
     'Cerera':['Rimska boginja ljubezni in pritlikavi planet', 'Samo rimska boginja ljubezni', 'Egipčanska boginja neba', 'Grška boginja zdravilstva'],
-    'bil vzdevek nemškega generalfeldmaršala Erwina Rommla':['Puščavska lisica', 'Afriški lev', 'Puščavski tiger', 'Nemški panter'],
+    'bil vzdevek generala Erwina Rommla':['Puščavska lisica', 'Afriški lev', 'Puščavski tiger', 'Nemški panter'],
     'Tar':['Azijska divja koza', 'Tibetansko govedo', 'Afriška volovska antilopa', 'Pritlikav indonezijski konj']
 }
 
