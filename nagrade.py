cef72e319b4bfafb89549ecac0b7d429ea516b1e
Urban Merhar <merhar.urban@gmail.com>
2019-08-08 10:35:31 +0200

    Dodal še dve nagradi.

--- nagrade.py
+++ nagrade.py
@@ -3,12 +3,14 @@ import random
 
 seznam_sal = [
     'Kaj dobiš, če daš dva polža skupaj? Celža!',
-    'Kateri avto gre najhitreje po makadamu? Služben!'
+    'Kateri avto gre najhitreje po makadamu? Služben!',
+    'Chuck Norris je posnel izumitev kamere!'
     
 ]
 
 seznam_zanimivosti = [
-    'Fobos je Marsov naravni satelit, ki ima nestabilno orbito in se zato na približno vsakih 100 let Marsu približa za 9m.'
+    'Fobos je Marsov naravni satelit, ki ima nestabilno tirnico in se zato na približno vsakih 100 let Marsu približa za 9m.',
+    'Največji proizvajalec pnevmatik na svetu je Lego.'
 
 ]
 
